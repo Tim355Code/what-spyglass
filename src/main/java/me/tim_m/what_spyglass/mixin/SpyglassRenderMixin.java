6fb5dca1b9ccce7c1db9998490959204c452bd18
Tim_M <tmerk355@gmail.com>
2026-03-19 00:16:07 +0100

    Removed unnecessary import.

--- src/main/java/me/tim_m/what_spyglass/mixin/SpyglassRenderMixin.java
+++ src/main/java/me/tim_m/what_spyglass/mixin/SpyglassRenderMixin.java
@@ -16,7 +16,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(InGameHud.class)
 public class SpyglassRenderMixin {
