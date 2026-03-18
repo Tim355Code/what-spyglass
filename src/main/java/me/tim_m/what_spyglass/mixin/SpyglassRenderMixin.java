d6906ce5d133883d51fc602ae73715d88da6fc22
Tim_M <tmerk355@gmail.com>
2026-03-18 23:29:10 +0100

    RenderMixin now also hides titles.

--- src/main/java/me/tim_m/what_spyglass/mixin/SpyglassRenderMixin.java
+++ src/main/java/me/tim_m/what_spyglass/mixin/SpyglassRenderMixin.java
@@ -5,8 +5,11 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import me.tim_m.what_spyglass.SpyglassStopCallback;
+import org.jetbrains.annotations.Nullable;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,6 +22,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class SpyglassRenderMixin {
     @Shadow @Final private MinecraftClient client;
+    @Shadow @Nullable private Text title;
+
+    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/hud/InGameHud;title:Lnet/minecraft/text/Text;", opcode = Opcodes.GETFIELD))
+    Text hideTitleWhileScoped(InGameHud hud) {
+        if (WhatSpyglassClient.inSpyglass) {
+            return null;
+        }
+        return title;
+    }
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/PlayerListHud;render(Lnet/minecraft/client/util/math/MatrixStack;ILnet/minecraft/scoreboard/Scoreboard;Lnet/minecraft/scoreboard/ScoreboardObjective;)V"))
     void stopPlayerListRender(
